4837adde59f6fa6607ee9208f3b7ad3af25e8059
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 19:58:04 -0400

    Create GetScore.java

---
+++ src/no/runsafe/peeveeeee/commands/GetScore.java
@@ -0,0 +1,27 @@
+package no.runsafe.peeveeeee.commands;
+
+import no.runsafe.framework.api.command.argument.IArgumentList;
+import no.runsafe.framework.api.command.player.PlayerCommand;
+import no.runsafe.framework.api.database.IRow;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.peeveepeearena.repositories.PlayerScoresRepository;
+
+public class GetScore extends PlayerCommand
+{
+	public GetScore(PlayerScoresRepository scoresRepository)
+	{
+		super("score", "Returns your PvE kill/death count", "runsafe.peeveeeee.score.get");
+		this.scoresRepository = scoresRepository;
+	}
+
+	@Override
+	public String OnExecute(IPlayer executor, IArgumentList parameters)
+	{
+		IRow scores = this.scoresRepository.getScores(executor);
+		int kills = (scores.Integer("kills") != null ? scores.Integer("kills") : 0);
+		int deaths = (scores.Integer("deaths") != null ? scores.Integer("deaths") : 0);
+
+		return String.format("&fYou have &a%s&f kills and &c%s&f deaths.", kills, deaths);
+	}
+
+	private final PlayerScoresRepository scoresRepository;
