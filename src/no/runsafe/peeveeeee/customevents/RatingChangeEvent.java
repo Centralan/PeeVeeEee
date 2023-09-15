7c6322666d2288859daa1b98acf5f88c13a87773
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:26:17 -0400

    Create RatingChangeEvent.java

---
+++ src/no/runsafe/peeveeeee/customevents/RatingChangeEvent.java
@@ -0,0 +1,20 @@
+package no.runsafe.peeveepeearena.customevents;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.event.player.RunsafeCustomEvent;
+
+public class RatingChangeEvent extends RunsafeCustomEvent
+{
+	public RatingChangeEvent(IPlayer player, Integer rating)
+	{
+		super(player, "peeveepee.rating.change");
+		this.rating = rating;
+	}
+
+	@Override
+	public Integer getData()
+	{
+		return this.rating;
+	}
+
+	private final Integer rating;
