8dc7b06d015c0505e67af6285dee9cc5e61eb47c
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:35:00 -0400

    Create StrengthBoost.java

---
+++ src/no/runsafe/peeveeeee/customitems/effects/StrengthBoost.java
@@ -0,0 +1,13 @@
+package no.runsafe.peeveeeeearena.customitems.effects;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Buff;
+
+public class StrengthBoost implements ICustomItemEffect
+{
+	@Override
+	public void performEffect(IPlayer player)
+	{
+		Buff.Combat.Damage.Increase.amplification(1).duration(20).applyTo(player);
+	}
+}
