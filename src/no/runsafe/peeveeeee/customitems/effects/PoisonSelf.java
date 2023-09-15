7217ef90e4d191d2bfb979e010848a3b779057b2
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:34:01 -0400

    Create PoisonSelf.java

---
+++ src/no/runsafe/peeveeeee/customitems/effects/PoisonSelf.java
@@ -0,0 +1,13 @@
+package no.runsafe.peeveeeeearena.customitems.effects;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Buff;
+
+public class PoisonSelf implements ICustomItemEffect
+{
+	@Override
+	public void performEffect(IPlayer player)
+	{
+		Buff.Combat.Damage.Poison.amplification(1).duration(10).applyTo(player);
+	}
+}
