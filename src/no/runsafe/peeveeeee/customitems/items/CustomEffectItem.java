df27ad8f1777da32bd31843c2034b3857201bf03
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:31:37 -0400

    Create CustomEffectItem.java

---
+++ src/no/runsafe/peeveeeee/customitems/items/CustomEffectItem.java
@@ -0,0 +1,21 @@
+package no.runsafe.peeveepeearena.customitems.items;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.peeveepeearena.customitems.effects.ICustomItemEffect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomEffectItem
+{
+	public void applyRandomEffect(IPlayer player)
+	{
+		this.itemEffects.get((int) (Math.random() * itemEffects.size())).performEffect(player);
+	}
+
+	public void addEffect(ICustomItemEffect effect)
+	{
+		this.itemEffects.add(effect);
+	}
+
+	private final List<ICustomItemEffect> itemEffects = new ArrayList<ICustomItemEffect>();
