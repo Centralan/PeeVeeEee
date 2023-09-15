915e8cd13f047c519d7a1b6263b55b6fa39b16f8
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:22:37 -0400

    Create SkipRounds.java

---
+++ src/no/runsafe/peeveeeee/commands/SkipRounds.java
@@ -0,0 +1,49 @@
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import com.garbagemule.MobArena.MobArena;
+import com.garbagemule.MobArena.framework.Arena;
+
+public class SkipRoundAndKillMobsCommand implements CommandExecutor {
+
+    private MobArena mobArena;
+
+    public SkipRoundAndKillMobsCommand(MobArena mobArena) {
+        this.mobArena = mobArena;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        Arena arena = mobArena.getArenaMaster().getArenaWithPlayer(player);
+
+        if (arena == null) {
+            sender.sendMessage("You are not in a MobArena.");
+            return true;
+        }
+
+        // Implement your logic to check if the player is in a specific region within the MobArena world
+        if (!isInSpecificRegion(player, arena)) {
+            sender.sendMessage("You are not in the specified region in the MobArena.");
+            return true;
+        }
+
+        // Implement your logic to skip rounds and kill all mobs here
+        // Example: arena.stopArena(); to stop the current round
+
+        sender.sendMessage("Round skipped, and all mobs killed!");
+        return true;
+    }
+
+    // Implement the method to check if the player is in a specific region within the MobArena world
+    private boolean isInSpecificRegion(Player player, Arena arena) {
+        // Your logic to check regions here, use MobArena's API and world edit/WorldGuard if needed
+        return false; // Replace with your implementation
+    }
+}
