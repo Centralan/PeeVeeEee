565479aa72f3a91e56e4d2391609671b4b5dcc30
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:01:24 -0400

    Create Teleport.java

---
+++ src/no/runsafe/peeveeeee/commands/Teleport.java
@@ -0,0 +1,23 @@
+package no.runsafe.peeveepeearena.commands;
+
+import no.runsafe.framework.api.command.argument.IArgumentList;
+import no.runsafe.framework.api.command.player.PlayerCommand;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.peeveepeearena.pvpporter.TeleportEngine;
+
+public class Teleport extends PlayerCommand
+{
+	public Teleport(TeleportEngine teleportEngine)
+	{
+		super("teleport", "Teleports you to the PvP arena", "runsafe.peeveepee.teleport");
+		this.teleportEngine = teleportEngine;
+	}
+
+	@Override
+	public String OnExecute(IPlayer executor, IArgumentList parameters)
+	{
+		this.teleportEngine.teleportToArena(executor);
+		return null;
+	}
+
+	private final TeleportEngine teleportEngine;
