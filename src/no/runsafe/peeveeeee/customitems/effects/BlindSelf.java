a2d90a7e9a4105711d2772fdd8a1c373989b999e
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:33:29 -0400

    Create BlindSelf.java

---
+++ src/no/runsafe/peeveeeee/customitems/effects/BlindSelf.java
@@ -0,0 +1,13 @@
+package no.runsafe.peeveeeeearena.customitems.effects;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Buff;
+
+public class BlindSelf implements ICustomItemEffect
+{
+	@Override
+	public void performEffect(IPlayer player)
+	{
+		Buff.Combat.Blindness.amplification(20).duration(10).applyTo(player);
+	}
+}
