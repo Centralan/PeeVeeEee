e33814f5287359d3e4cefca132528be65f74fd9b
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:46:12 -0400

    Create SignChange.java

---
+++ src/no/runsafe/peeveeeee/events/SignChange.java
@@ -0,0 +1,18 @@
+package no.runsafe.peeveeeee.events;
+
+import no.runsafe.framework.api.block.IBlock;
+import no.runsafe.framework.api.event.block.ISignChange;
+import no.runsafe.framework.api.player.IPlayer;
+
+public class SignChange implements ISignChange
+{
+	@Override
+	public boolean OnSignChange(IPlayer player, IBlock block, String[] text)
+	{
+		if (text[0].equalsIgnoreCase("[PVP Warp]"))
+			if (!player.hasPermission("runsafe.peeveeee.teleport.create"))
+				return false;
+
+		return true;
+	}
+}
