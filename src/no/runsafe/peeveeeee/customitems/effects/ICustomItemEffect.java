414cadfdd48c4158e39f64f27756234f2cbbff00
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:35:27 -0400

    Create ICustomItemEffect.java

---
+++ src/no/runsafe/peeveeeee/customitems/effects/ICustomItemEffect.java
@@ -0,0 +1,8 @@
+package no.runsafe.peeveepeearena.customitems.effects;
+
+import no.runsafe.framework.api.player.IPlayer;
+
+public interface ICustomItemEffect
+{
+	public void performEffect(IPlayer player);
+}
