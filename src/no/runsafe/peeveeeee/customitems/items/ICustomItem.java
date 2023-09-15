b25b1c8626711d75947649759e4400f0e4f3a705
Centralan <Jessegauthier070296@gmail.com>
2023-09-14 20:30:48 -0400

    Create ICustomItem.java

---
+++ src/no/runsafe/peeveeeee/customitems/items/ICustomItem.java
@@ -0,0 +1,13 @@
+package no.runsafe.peeveeeeearena.customitems.items;
+
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public interface ICustomItem
+{
+	public void onUse(IPlayer player);
+
+	public String getItemName();
+
+	public RunsafeMeta getItem();
+}
